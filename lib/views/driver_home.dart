0b180b833b05acb2fac01a68b0e53593da8e9190
Aditi Maurya <imt_2019006@iiitm.ac.in>
2021-11-24 16:03:53 +0530

    Refactor driver home view
    Signed-off-by: Aditi Maurya <imt_2019006@iiitm.ac.in>

--- lib/views/driver_home.dart
+++ lib/views/driver_home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emergency_notifier/common/constants.dart';
 import 'package:emergency_notifier/models/auth_model.dart';
 import 'package:emergency_notifier/services/emergency_service.dart';
+import 'package:emergency_notifier/widgets/driver_home_card.dart';
 import 'package:emergency_notifier/widgets/image_carousel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -37,72 +38,11 @@ class _DriverHomeState extends State<DriverHome> {
               itemBuilder: (context, index) {
                 final emergency = emergencies?[index];
 
-                return Card(
-                  elevation: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(defaultPadding),
-                    child: Column(
-                      children: [
-                        Text(
-                          '${(emergency?.data() as dynamic)['address']}',
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        const SizedBox(height: defaultPadding / 2),
-                        Chip(
-                          label: Text(
-                            'Severity: ${(emergency?.data() as dynamic)['severity']}',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                          backgroundColor: primaryColor,
-                          elevation: 0,
-                          shadowColor: Colors.grey[60],
-                          padding: const EdgeInsets.all(defaultPadding / 2),
-                        ),
-                        const SizedBox(height: defaultPadding / 2),
-                        Text(
-                          '${(emergency?.data() as dynamic)['description']}',
-                          style: Theme.of(context).textTheme.caption,
-                          textAlign: TextAlign.justify,
-                        ),
-                        const SizedBox(height: defaultPadding),
-                        ImageCarousel(
-                          imageList: (emergency?.data() as dynamic)['images'],
-                        ),
-                        const SizedBox(height: defaultPadding),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: (emergency?.data()
-                                        as dynamic)['handledBy'] !=
-                                    authModel.uid
-                                ? () {
-                                    handleEmergency(authModel, emergency!.id);
-                                  }
-                                : (emergency?.data() as dynamic)['status'] ==
-                                        'handled'
-                                    ? null
-                                    : () {
-                                        markHandledEmergency(
-                                            authModel, emergency!.id);
-                                      },
-                            child: (emergency?.data()
-                                        as dynamic)['handledBy'] ==
-                                    authModel.uid
-                                ? (emergency?.data() as dynamic)['status'] ==
-                                        'handled'
-                                    ? const Text('Handled')
-                                    : const Text('Mark as Handled')
-                                : const Text('Accept Emergency'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return DriverHomeCard(
+                    emergency: emergency,
+                    authModel: authModel,
+                    handleEmergency: handleEmergency,
+                    markHandledEmergency: markHandledEmergency);
               },
             ),
           );
